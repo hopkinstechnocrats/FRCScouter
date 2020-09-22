0e5d48425b49bb360b447f6328e42018d1e83ddf
pizzajess <40721768+pizzajess@users.noreply.github.com>
2020-09-22 17:32:40 -0500

    old site recovery via library change

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,30 @@
 // FRCScouter is not snake case, but we want it that way
 #![allow(non_snake_case)]
 
-// Import rocket macros with nightly tools
-#![feature(proc_macro_hygiene, decl_macro)]
-extern crate rocket;
-
-// Import the static hosting tool
-use rocket_contrib::serve::StaticFiles;
 // WebSockets, `roboconnect`, and rocket all run in parallel
 use std::thread;
 
+use actix_web::*;
+
+use actix_files as fs;
+
 mod server;
 
-fn main() {
-    // Create and launch rocket website (see /static), on localhost::[rocket.toml]
-    // This is done in a new thread
-    let website = thread::spawn(move || {
-        rocket::ignite()
-            .mount("/", StaticFiles::from("static"))
-            .launch();
-    });
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
 
     // Launch websocket server in a new thread
     thread::spawn(move || {
         server::launch_websocket();
     });
 
-    // In the event that `server::launch_websocket()` somehow exits,
-    // we block until user force quits (this area *should* be unreachable)
-    match website.join() {
-        Ok(_) => {
-            println!("Sucsesfully exited.");
-        }
-        Err(_) => {
-            println!("An error occured joining website thread to main thread.");
-        }
-    }
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    env_logger::init();
+
+    println!("launching server");
+    // Create and launch website on localhost::80
+    HttpServer::new(|| {
+        App::new().service(fs::Files::new("/", "./static").show_files_listing())
+    }).bind("0.0.0.0:80")?
+    .run().await
 }
