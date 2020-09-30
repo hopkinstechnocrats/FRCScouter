4586f0b51a971656e02e60c4e3f4888b01534c50
pizzajess <40721768+pizzajess@users.noreply.github.com>
2020-09-30 18:13:14 -0500

    first JSON test

--- src/backend.rs
+++ src/backend.rs
@@ -3,7 +3,7 @@ use ws::listen;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-use crate::data::ServerData;
+use crate::data::*;
 
 /// Launches the WebSocket server. This communicates with clients and processes their data.
 pub fn launch() {
@@ -11,12 +11,15 @@ pub fn launch() {
     // threads for use in multiple places at once.
     let server = Arc::new(
         Mutex::new(
-            ServerData::new()
+            Data::new()
         )
     );
 
     // Listen on an address and call the closure for each connection
     if let Err(error) = listen("0.0.0.0:81", |out| {
+        // prepare the server to be moved into thread if needed
+        let server = Arc::clone(&server);
+
         // Someone has connected! Let us deal with the data and such
         // The handler needs to take ownership of out, so we use move
         move |msg: ws::Message| {
@@ -37,8 +40,47 @@ pub fn launch() {
                         Ok(data) => {
                             if let Some(response) = data["type"].as_str() {
                                 match response {
-                                    "GetUSID" => {
-                                        // not done
+                                    "attempted-shot" => {
+                                        let shot_location: ShotLocation;
+                                        let bin_location: BinLocation;
+                                        let shot_succeded: Succeeded;
+                                        if let Some(sl) = data["shot-location"].as_str() {
+                                            if let Some(slc) = ShotLocation::from_str(sl) {
+                                                shot_location = slc;
+                                            }
+                                            else {
+                                                return out.send("{\"result\": \"malformed\"}");
+                                            }
+                                        }
+                                        else {
+                                            return out.send("{\"result\": \"malformed\"}");
+                                        }
+                                        if let Some(bl) = data["bin-location"].as_str() {
+                                            if let Some(blc) = BinLocation::from_str(bl) {
+                                                bin_location = blc
+                                            }
+                                            else {
+                                                return out.send("{\"result\": \"malformed\"}");
+                                            }
+                                        }
+                                        else {
+                                            return out.send("{\"result\": \"malformed\"}");
+                                        }
+                                        if let Some(ss) = data["shot-succeeded"].as_str() {
+                                            if let Some(ssc) = Succeeded::from_str(ss) {
+                                                shot_succeded = ssc;
+                                            }
+                                            else {
+                                                return out.send("{\"result\": \"malformed\"}");
+                                            }
+                                        }
+                                        else {
+                                            return out.send("{\"result\": \"malformed\"}");
+                                        }
+                                        let mut server = server.lock().unwrap();
+                                        server.attempted_shot(shot_location, bin_location, shot_succeded);
+                                        drop(server);
+                                        return out.send("{\"result\": \"ok\"}");
                                     },
                                     _ => {
                                         println!("Warning: A message was sent to the WebSocket server with an unknown type label.");
--- src/data.rs
+++ src/data.rs
@@ -3,37 +3,87 @@
 
 #[derive(Clone, Eq, PartialEq, Debug)]
 /// The WebSocket server's state and storage
-pub struct ServerData {
-    /// represents the highest unique ID handed out by the server.
-    usid: usize,
-    /// represents all the received data on the server.
-    pub data: GameData,
+pub struct Data {
+    attempted_shots: Vec<(ShotLocation, BinLocation, Succeeded, Timestamp)>
 }
 
-impl ServerData {
-    /// Creates a new ServerData
-    pub fn new() -> ServerData {
-        ServerData {
-            usid: 0,
-            data: GameData::new(),
+impl Data {
+    /// Creates a new Data instance
+    pub fn new() -> Data {
+        Data {
+            attempted_shots: vec![]
         }
     }
-    /// Gets a unique USID (User Session IDentification) and increments the interal USID counter
-    pub fn get_next_usid(&mut self) -> usize {
-        self.usid += 1;
-        return self.usid;
+    pub fn attempted_shot(&mut self, sl: ShotLocation, bl: BinLocation, su: Succeeded) {
+        self.attempted_shots.push((sl, bl, su, Timestamp::now()));
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub enum ShotLocation {
+    BlueGoalLeft,
+    BlueGoalRight,
+    CenterLeft,
+    CenterRight,
+    RedGoalLeft,
+    RedGoalRight,
+}
+
+impl ShotLocation {
+    pub fn from_str(from: &str) -> Option<ShotLocation> {
+        match from.to_lowercase().as_str() {
+            "bluegoalleft" => return Some(ShotLocation::BlueGoalLeft),
+            "bluegoalright" => return Some(ShotLocation::BlueGoalRight),
+            "centerleft" => return Some(ShotLocation::CenterLeft),
+            "centerright" => return Some(ShotLocation::CenterRight),
+            "redgoalleft" => return Some(ShotLocation::RedGoalLeft),
+            "redgoalright" => return Some(ShotLocation::RedGoalRight),
+            _ => return None
+        }
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub enum BinLocation {
+    High,
+    Low
+}
+
+impl BinLocation {
+    pub fn from_str(from: &str) -> Option<BinLocation> {
+        match from.to_lowercase().as_str() {
+            "high" => return Some(BinLocation::High),
+            "low" => return Some(BinLocation::Low),
+            _ => return None
+        }
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub enum Succeeded {
+    True,
+    False
+}
+
+impl Succeeded {
+    pub fn from_str(from: &str) -> Option<Succeeded> {
+        match from.to_lowercase().as_str() {
+            "true" => return Some(Succeeded::True),
+            "false" => return Some(Succeeded::False),
+            _ => return None
+        }
     }
 }
 
 #[derive(Clone, Eq, PartialEq, Debug)]
-pub struct GameData {
+pub struct Timestamp {
 
 }
 
-impl GameData {
-    pub fn new() -> GameData {
-        GameData {
-
+impl Timestamp {
+    pub fn now() -> Timestamp {
+        Timestamp {
+            
         }
     }
 }
